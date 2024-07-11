c1cafef7cbdba0956b7f227248f649388777d4e4
Qingyuan Pei <qpei@uoguelph.ca>
2024-07-11 15:41:57 -0400

    prepare input for r

---
+++ ComputeCan/10_Prepare_Input_R.sh
@@ -0,0 +1,10 @@
+### This script is designed to prepare proper input format for R
+### Author: Mia Pei
+### Last Modified: July 11, 2024
+
+# Remove the first block of text from each annotated file
+cd /home/miapei/projects/def-gagnoned/miapei/BINF6999_ComputeCan/RE2_output_20240619/RE2_manual_annotation
+for f in annotated_*.csv;
+do (tail -n +7 $f > ${f%_CLUSTER_TABLE.csv}_MiaPei.csv);
+done
+
